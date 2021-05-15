5cd681c7a8186c62d20bd0a0f6896c078c0411a2
Joshua <jshggagnier@gmail.com>
2021-05-14 19:01:55 -0700

    This time the updater actually works, read it for details!

--- UpdateMyLocal.bat
+++ UpdateMyLocal.bat
@@ -1,4 +1,6 @@
-cd ..
+rmdir evening-castle-67258 /s
 heroku git:clone -a evening-castle-67258
 echo "mission accomplished?"
 pause
+pretty sure anything after this point never gets run, so I can let you know how to use this!
+Basically, just move this to the directory directly above your repo, then run the bat file, and it will wipe the repo out, you could also just run this wherever you want your repo to be set up!
